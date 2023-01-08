c52e73ad741f3c78db831b9019bda007d594aeb1
Vinicius Santos <vinicius.v.sts@gmail.com>
2023-01-08 00:43:38 -0300

    Fusion test

--- src/main/java/vvsantos/fake/oauth/loop/MainLoopJwtTest.java
+++ src/main/java/vvsantos/fake/oauth/loop/MainLoopJwtTest.java
@@ -1,11 +1,10 @@
 package vvsantos.fake.oauth.loop;
 
 import vvsantos.fake.oauth.loop.service.JwtService;
-import vvsantos.fake.oauth.loop.service.RsKeyService;
 
 public class MainLoopJwtTest {
     public static void main(String[] args) {
-        var jwtService = new JwtService(new RsKeyService());
+        var jwtService = new JwtService();
         var initialTime = System.currentTimeMillis();
         for (int i = 0; i < 10_000; i++) {
             var token = jwtService.getFakeToken();
--- src/main/java/vvsantos/fake/oauth/loop/service/JwtService.java
+++ src/main/java/vvsantos/fake/oauth/loop/service/JwtService.java
@@ -1,33 +1,35 @@
 package vvsantos.fake.oauth.loop.service;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
+import io.fusionauth.jwt.rsa.RSASigner;
 
-import java.security.interfaces.RSAPrivateKey;
-import java.time.Instant;
-import java.util.Date;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 
 public class JwtService {
 
-    private Algorithm algorithm;
+    private static final String KEY_PKCS1_PATH = "src/main/resources/key1.pem";
+    private RSASigner signer;
 
-    public JwtService(RsKeyService rsKeyService) {
+    public JwtService() {
         try {
-            rsKeyService.init();
-            algorithm = Algorithm.RSA256(null, (RSAPrivateKey) rsKeyService.getKeyPair().getPrivate());
-        } catch (Exception e) {
+            signer = RSASigner.newSHA256Signer(new String(Files.readAllBytes(Paths.get(KEY_PKCS1_PATH))));
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public String getFakeToken() {
-        return JWT.create()
-                .withClaim("cid", "vvsantos")
-                .withClaim("scope", "SCOPE1:GET SCOPE1:POST SCOPE1:PUT SCOPE2:GET SCOPE2:POST SCOPE2:PUT")
-                .withIssuer("test")
-                .withExpiresAt(Date.from(Instant.now().plusMillis(5000)))
-                .withIssuedAt(Date.from(Instant.now()))
-                .sign(algorithm);
+        io.fusionauth.jwt.domain.JWT jwt = new io.fusionauth.jwt.domain.JWT()
+                .setIssuedAt(ZonedDateTime.now(ZoneOffset.UTC))
+                .setIssuer("test")
+                .setExpiration(ZonedDateTime.now(ZoneOffset.UTC).plusMinutes(60))
+                .addClaim("cid", "vvsantos")
+                .addClaim("scope", "SCOPE1:GET SCOPE1:POST SCOPE1:PUT SCOPE2:GET SCOPE2:POST SCOPE2:PUT");
+
+        return io.fusionauth.jwt.domain.JWT.getEncoder().encode(jwt, signer);
     }
 
 
